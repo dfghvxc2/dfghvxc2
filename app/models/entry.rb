d1b78377752b35444ff44ef4a08f7db9a8fba3e2
dfghvxc2 <120800417+dfghvxc2@users.noreply.github.com>
2022-12-18 11:00:27 +0000

    Update validates

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,9 @@
 class Entry < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :published_at, presence: true
+    validates :image, presence: true
+
+
+
 end
