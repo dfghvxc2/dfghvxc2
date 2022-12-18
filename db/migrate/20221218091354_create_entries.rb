1cc104a4914d645dcb980e34ef7b046f96a6bd1e
dfghvxc2 <120800417+dfghvxc2@users.noreply.github.com>
2022-12-18 09:26:33 +0000

    Create entries and run migration

---
+++ db/migrate/20221218091354_create_entries.rb
@@ -0,0 +1,11 @@
+class CreateEntries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entries do |t|
+      t.string :title
+      t.text :content
+      t.datetime :published_at
+
+      t.timestamps
+    end
+  end
+end
